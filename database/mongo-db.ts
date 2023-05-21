52ff889d2fb13705513b4cab343b65eaed356856
unknown <bryanlin107@gmail.com>
2023-05-20 22:56:38 -0400

    fix

--- database/mongo-db.ts
+++ database/mongo-db.ts
@@ -2,10 +2,10 @@ import mongoose from "mongoose";
 
 export default async function connectToDb() {
   if (mongoose.connection.readyState >= 1) return;
-  return mongoose.connect(
-    `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@unitywear.hcmo8.mongodb.net/${db}?retryWrites=true&w=majority`,
-    {
-      autoIndex: true,
-    }
-  );
+  // return mongoose.connect(
+  //   `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@unitywear.hcmo8.mongodb.net/${db}?retryWrites=true&w=majority`,
+  //   {
+  //     autoIndex: true,
+  //   }
+  // );
 }
