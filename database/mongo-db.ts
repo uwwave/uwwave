ff4b0138833fc93a34a2c7698089786a68b4661c
unknown <bryanlin107@gmail.com>
2023-05-20 23:25:04 -0400

    first

--- database/mongo-db.ts
+++ database/mongo-db.ts
@@ -1,7 +1,7 @@
-import mongoose from "mongoose";
+// import mongoose from "mongoose";
 
 export default async function connectToDb() {
-  if (mongoose.connection.readyState >= 1) return;
+  // if (mongoose.connection.readyState >= 1) return;
   // return mongoose.connect(
   //   `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@unitywear.hcmo8.mongodb.net/${db}?retryWrites=true&w=majority`,
   //   {
