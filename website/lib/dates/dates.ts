a90f9718e132863577673e078b3864fb8e639294
a28dong <andrewdong1@gmail.com>
2023-06-11 18:08:14 -0400

    update search button, move utility

--- website/lib/dates/dates.ts
+++ website/lib/dates/dates.ts
@@ -14,3 +14,34 @@ export const calculateDaysFromNow = (date: Date): string => {
     return daysDifference.toString();
   }
 };
+
+export const getTimeDiffString = (timeOld: string) => {
+  const timeDiffSeconds = moment().utc().diff(timeOld, "second");
+  let timeDiffString;
+  if (timeDiffSeconds === 1) {
+    // 1 s
+    timeDiffString = "1 second ago ";
+  } else if (timeDiffSeconds < 60) {
+    // < 1 min in seconds
+    timeDiffString = `${moment().utc().diff(timeOld, "second")} seconds ago `;
+  } else if (timeDiffSeconds < 119) {
+    // 1 min
+    timeDiffString = "1 minute ago ";
+  } else if (timeDiffSeconds < 3600) {
+    // < 1 hr in minutes
+    timeDiffString = `${moment().utc().diff(timeOld, "minute")} minutes ago `;
+  } else if (timeDiffSeconds < 7199) {
+    // 1 hr
+    timeDiffString = "1 hour ago ";
+  } else if (timeDiffSeconds < 86400) {
+    // < 1 day in hours
+    timeDiffString = `${moment().utc().diff(timeOld, "hour")} hours ago `;
+  } else if (timeDiffSeconds < 172799) {
+    // 1 day
+    timeDiffString = "1 day ago ";
+  } else {
+    // >= 2 days
+    timeDiffString = `${moment().utc().diff(timeOld, "day")} days ago `;
+  }
+  return timeDiffString;
+};
