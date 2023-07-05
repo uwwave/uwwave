727ce21e5e3c543bc21bb12b6d386902ae0c652f
bryanling1 <bryanling107@gmail.com>
2023-07-05 01:59:27 -0400

    Test deploy

--- website/pages/api/admin/metrics/extensionInstalls.ts
+++ website/pages/api/admin/metrics/extensionInstalls.ts
@@ -1,8 +1,7 @@
 import type { NextApiRequest, NextApiResponse } from "next";
-import connectToDb from "src/database/mongo-db";
 import { getServerSession } from "next-auth";
 import { nextAuthOptions } from "src/pages/api/auth/[...nextauth]";
-import puppeteer from "puppeteer";
+import chromium from "chrome-aws-lambda";
 
 const handler = async (req: NextApiRequest, res: NextApiResponse) => {
   try {
@@ -14,6 +13,7 @@ const handler = async (req: NextApiRequest, res: NextApiResponse) => {
         res.status(404).end();
     }
   } catch (err) {
+    console.log(err);
     res.status(400).end(err);
   }
 };
@@ -26,8 +26,14 @@ const handleGet = async (req: NextApiRequest, res: NextApiResponse) => {
   if (!user.isAdmin) {
     throw "Must be signed in admin";
   }
-  await connectToDb();
-  const browser = await puppeteer.launch({ headless: "new" });
+
+  const browser = await chromium.puppeteer.launch({
+    args: chromium.args,
+    defaultViewport: chromium.defaultViewport,
+    executablePath: await chromium.executablePath,
+    headless: chromium.headless,
+    ignoreHTTPSErrors: true,
+  });
   const page = await browser.newPage();
 
   // Navigate to the extension page
@@ -39,7 +45,7 @@ const handleGet = async (req: NextApiRequest, res: NextApiResponse) => {
   await page.waitForSelector(".e-f-ih");
 
   // Get the installation count value
-  const installationCount = await page.$eval(".e-f-ih", element => {
+  const installationCount = await page.$eval(".e-f-ih", (element: any) => {
     const countText = element.textContent?.replace(/[^\d]/g, "");
     return countText ? parseInt(countText, 10) : 0;
   });
